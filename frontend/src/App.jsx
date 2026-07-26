8de12f90781570acc4ab4622ef8088be3380ab4a
Bishalsingh153 <bishalsinghofcp@gmail.com>
2026-07-26 06:25:40 +0530

    Setting Page themes using DaisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import { axiosInstance } from './lib/axios';
 import { useAuthStore } from './store/useAuthStore';
 import {Loader} from "lucide-react"
 import {Toaster} from "react-hot-toast"
+import { useThemeStore } from './store/useThemeStore';
 
 
 
@@ -19,6 +20,7 @@ import {Toaster} from "react-hot-toast"
 const App = () => {
 
  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
+ const {theme} = useThemeStore()
 
  useEffect(()=>{
   checkAuth()
@@ -34,7 +36,7 @@ const App = () => {
  )
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar/>
 
       <Routes>
