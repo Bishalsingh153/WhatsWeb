4dd285af06274a149cdf9829e8652e16fa7dc557
Bishalsingh153 <bishalsinghofcp@gmail.com>
2026-07-31 13:41:34 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
     credentials: true 
 }))
 
@@ -27,10 +29,9 @@ app.use("/api/messages",messageRoutes)
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    
 
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 
